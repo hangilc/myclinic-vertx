c1f272e1695b377ba9a61e43bc40fd4fdf4ad8b4
hangilc <hangil@chang.jp>
2020-05-05 15:43:10 +0900

    adding rest handler

--- server/src/main/java/dev/myclinic/vertx/server/Main.java
+++ server/src/main/java/dev/myclinic/vertx/server/Main.java
@@ -1,24 +1,39 @@
 package dev.myclinic.vertx.server;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.myclinic.vertx.db.MysqlDataSourceConfig;
+import dev.myclinic.vertx.db.MysqlDataSourceFactory;
+import dev.myclinic.vertx.db.TableSet;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpServer;
 import io.vertx.ext.web.Route;
 import io.vertx.ext.web.Router;
 
+import javax.sql.DataSource;
+
 public class Main {
 
     public static void main(String[] args){
+        MysqlDataSourceConfig mysqlConfig = new MysqlDataSourceConfig();
+        DataSource ds = MysqlDataSourceFactory.create(mysqlConfig);
+        TableSet ts = TableSet.create();
+        ObjectMapper mapper = createObjectMapper();
         Vertx vertx = Vertx.vertx();
         HttpServer server = vertx.createHttpServer();
         Router router = Router.router(vertx);
         Route restRoute = router.route("/json/:action");
-        restRoute.blockingHandler(new RestHandler());
+        restRoute.blockingHandler(new RestHandler(ds, ts, mapper));
         server.requestHandler(router);
         server.webSocketHandler(ws -> {
             System.out.println("opened");
             ws.closeHandler(e -> System.out.println("closed"));
         });
         server.listen(28080);
+    }
+
+    private static ObjectMapper createObjectMapper(){
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper;
     }
 
 }
--- server/src/main/java/dev/myclinic/vertx/server/RestHandler.java
+++ server/src/main/java/dev/myclinic/vertx/server/RestHandler.java
@@ -1,24 +1,60 @@
 package dev.myclinic.vertx.server;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.myclinic.vertx.db.Backend;
+import dev.myclinic.vertx.db.Query;
+import dev.myclinic.vertx.db.TableSet;
+import dev.myclinic.vertx.dto.PatientDTO;
 import io.vertx.core.Handler;
 import io.vertx.core.MultiMap;
 import io.vertx.core.http.HttpServerRequest;
+import io.vertx.core.http.HttpServerResponse;
 import io.vertx.ext.web.RoutingContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
 
 class RestHandler implements Handler<RoutingContext> {
+
+    private static Logger logger = LoggerFactory.getLogger(RestHandler.class);
+
+    private DataSource ds;
+    private TableSet ts;
+    private ObjectMapper mapper;
+
+    public RestHandler(DataSource ds, TableSet ts, ObjectMapper mapper){
+        this.ds = ds;
+        this.ts = ts;
+        this.mapper = mapper;
+    }
 
     @Override
     public void handle(RoutingContext routingContext) {
         HttpServerRequest req = routingContext.request();
         String action = req.getParam("action");
         if( action.equals("get-patient") ){
+            Connection conn = null;
             try {
                 MultiMap params = req.params();
                 int patientId = Integer.parseInt(params.get("patient-id"));
-                routingContext.response().end(String.valueOf(patientId));
+                conn = ds.getConnection();
+                Query query = new Query(conn);
+                Backend backend = new Backend(ts, query);
+                PatientDTO patient = backend.getPatient(patientId);
+                HttpServerResponse resp = req.response();
+                resp.putHeader("content-type", "application/json; charset=UTF-8");
+                String result = mapper.writeValueAsString(patient);
+                resp.end(result);
             } catch(Exception e){
+                if( conn != null ){
+                    try {
+                        conn.rollback();
+                    } catch(Exception ex){
+                        logger.error("Rollback failed", ex);
+                    }
+                }
                 throw new RuntimeException(e);
             }
         } else {

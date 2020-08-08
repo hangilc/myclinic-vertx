c241ac9ce4a5519fce0f33c129498f721b3a9553
hangilc <hangil@chang.jp>
2020-08-08 16:29:00 +0900

    added dropdown menu

--- server/webroot/portal2/comp/dropdown.js
+++ server/webroot/portal2/comp/dropdown.js
@@ -1,42 +1,32 @@
 
-export function createDropdown(label, linkMap) {
-    return (function(){
-        let ele = document.createElement("div");
-        ele.classList.add("dropdown");
-        let a = document.createElement("a");
-        a.innerText = label;
-        a.href = "javascript:void(0)";
-        a.classList.add("dropdown-button");
-        ele.append(a);
-        let links = document.createElement("div");
-        links.classList.add("dropdown-links");
-        ele.append(links);
-        let backdrop = document.createElement("div");
-        backdrop.classList.add("dropdown-backdrop");
-        backdrop.onclick = () => {
+export function createDropdown(button, linkSpecs) { // linkSpecs: [{label, action}, ...]
+    let links = document.createElement("div");
+    links.classList.add("dropdown-links");
+    let backdrop = document.createElement("div");
+    backdrop.classList.add("dropdown-backdrop");
+    backdrop.onclick = () => {
+        links.style.display = "none";
+        backdrop.remove();
+    };
+    for (let spec of linkSpecs) {
+        let key = spec.label;
+        let act = spec.action;
+        let link = document.createElement("a");
+        link.innerText = key;
+        link.href = "javascript:void(0)";
+        link.onclick = event => {
             links.style.display = "none";
             backdrop.remove();
+            act();
         };
-        for (let key in linkMap) {
-            if( !linkMap.hasOwnProperty(key) ){
-                continue;
-            }
-            let act = linkMap[key];
-            let link = document.createElement("a");
-            link.innerText = key;
-            link.href = "javascript:void(0)";
-            link.onclick = event => {
-                links.style.display = "none";
-                backdrop.remove();
-                act();
-            };
-            links.append(link);
-        }
-        a.onclick = event => {
-            document.body.append(backdrop);
-            links.style.display = "block";
-        };
-        return ele;
-    })();
-
+        links.append(link);
+    }
+    button.onclick = event => {
+        document.body.append(backdrop);
+        let r = button.getBoundingClientRect();
+        links.style.left = `${r.left}px`;
+        links.style.top = `${r.bottom}px`;
+        document.body.append(links);
+        links.style.display = "block";
+    };
 }
--- server/webroot/portal2/practice/practice.js
+++ server/webroot/portal2/practice/practice.js
@@ -1,13 +1,38 @@
+import {parseElement} from "../js/parse-element.js";
+import {createDropdown} from "../comp/dropdown.js";
 
 let tmpl = `
 <h2>診察</h2>
 <div>
-    <a href="javascript:void(0)">患者選択▼</a>
+    <a href="javascript:void(0)" class="x-choose-patient">患者選択▼</a>
 </div>
 `;
 
 export function createPractice(rest){
     let ele = document.createElement("div");
     ele.innerHTML = tmpl;
+    let map = parseElement(ele);
+    createDropdown(map.choosePatient, [
+        {
+            label: "受付患者選択",
+            action: () => {}
+        },
+        {
+            label: "患者検索",
+            action: () => {}
+        },
+        {
+            label: "最近の診察",
+            action: () => {}
+        },
+        {
+            label: "本日の診察",
+            action: () => {}
+        },
+        {
+            label: "以前の診察",
+            action: () => {}
+        }
+    ]);
     return ele;
 }

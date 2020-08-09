1c7362701f7783359a2e070b154343a198c84177
hangilc <hangil@chang.jp>
2020-08-09 15:19:53 +0900

    added select patient

--- server/webroot/portal2/practice/practice.js
+++ server/webroot/portal2/practice/practice.js
@@ -3,6 +3,8 @@ import {createDropdown} from "../comp/dropdown.js";
 import {openSelectWqueueDialog} from "./select-wqueue-dialog.js";
 import {openSearchPatientDialog} from "./search-patient-dialog.js";
 import {openSelectRecentVisitDialog} from "./select-recent-visit-dialog.js";
+import {openTodaysVisitDialog} from "./select-todays-visit-dialog.js";
+import {openPrevVisitDialog} from "./select-prev-visit-dialog.js";
 
 let tmpl = `
 <h2>診察</h2>
@@ -55,11 +57,17 @@ function setupChoosePatient(ele, ctx){
         },
         {
             label: "本日の診察",
-            action: () => {}
+            action: async () => {
+                let visitPatient = await openTodaysVisitDialog(ctx.rest);
+                console.log("visit-patient", visitPatient);
+            }
         },
         {
             label: "以前の診察",
-            action: () => {}
+            action: async () => {
+                let visitPatient = await openPrevVisitDialog(ctx.rest);
+                console.log("visit-patient", visitPatient);
+            }
         }
     ]);
 }

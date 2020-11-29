5485a52bab295ff86139d77f20e631a001148673
hangilc <hangil@chang.jp>
2020-11-29 11:45:23 +0900

    added patient image preview

--- server/src/main/resources/webroot/components/patient-image-list.js
+++ server/src/main/resources/webroot/components/patient-image-list.js
@@ -16,6 +16,7 @@ let itemTmpl = `
 export class PatientImageList extends Widget {
     #rest;
     #patientId = 0;
+    #scale = 4.0;
 
     constructor(rest){
         super();
@@ -42,8 +43,8 @@ export class PatientImageList extends Widget {
         let value = this.bmap.select.querySelector("option:checked").value;
         let blob = await this.#rest.getPatientImageBlob(this.#patientId, value);
         let img = createImageFromBlob(blob);
-        img.width = 210;
-        img.height = 297;
+        img.width = 210 * this.#scale;
+        img.height = 297 * this.#scale;
         this.bmap.preview.innerHTML = "";
         this.bmap.preview.append(img);
     }

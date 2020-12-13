5bc477fb6efb449ec5d890d6e29edf36a48eea16
hangilc <hangil@chang.jp>
2020-12-13 22:28:28 +0900

    recovered paper-scan.js

---
+++ server/src/main/resources/webroot/js/paper-scan.js
@@ -0,0 +1,23 @@
+import * as kanjidate from "./kanjidate.js";
+
+export function getTimestamp() {
+    return kanjidate.getTimestamp();
+}
+
+export function getFileExtension(filename) {
+    let i = filename.lastIndexOf(".");
+    if (i < 0) {
+        return "";
+    } else {
+        return filename.substring(i);
+    }
+}
+
+export function createPaperScanFileName(patientId, tag, timestamp, ser, ext) {
+    if( ser == null || ser === "" ){
+        ser = "";
+    } else {
+        ser = `-${ser}`;
+    }
+    return `${patientId}-${tag}-${timestamp}${ser}${ext}`;
+}

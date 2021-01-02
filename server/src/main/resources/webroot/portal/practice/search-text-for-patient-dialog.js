ab0cd1878322685cbd6438bf9e4206eb39c41a2e
hangilc <hangil@chang.jp>
2021-01-02 16:04:03 +0900

    cleanup

--- server/src/main/resources/webroot/portal/practice/search-text-for-patient-dialog.js
+++ server/src/main/resources/webroot/portal/practice/search-text-for-patient-dialog.js
@@ -1,10 +1,6 @@
-import {SearchTextDialog} from "./search-text-dialog.js";
 import {parseElement} from "../../js/parse-node.js";
-import {Component} from "../js/component.js";
-import {TitleDisp} from "./title-disp.js";
-import {TextDisp} from "./text/text-disp.js";
 import {Dialog} from "../../js/dialog2.js";
-import {click, submit} from "../../js/dom-helper.js";
+import {submit} from "../../js/dom-helper.js";
 import {titleRep} from "./title/title-funs.js";
 import {createElementFrom} from "../../js/create-element-from.js";
 import {textRep} from "./text/text-funs.js";
@@ -77,47 +73,4 @@ class Item {
     }
 }
 
-class SearchTextForPatientDialogOrig extends SearchTextDialog {
-    constructor(ele, map, rest) {
-        super(ele, map, rest);
-        this.itemTemplate = map.itemTemplate;
-    }
-
-    init(dialogTitle){
-        super.init(dialogTitle);
-        this.itemTemplateHtml = this.itemTemplate.html();
-        this.onSearch((text, page) => this.doSearch(text, page));
-    }
-
-    set(patientId){
-        super.set();
-        this.patientId = patientId;
-    }
-
-    update(searchTextResult){
-        let page = searchTextResult.page;
-        let totalPages = searchTextResult.totalPages;
-        let textVisits = searchTextResult.textVisits;
-        this.nav.set(page + 1, totalPages);
-        this.resultElement.html("");
-        for(let textVisit of textVisits){
-            let compItem = this.createItem(textVisit);
-            compItem.appendTo(this.resultElement);
-        }
-    }
-
-    async doSearch(text, page){
-        let result = await this.rest.searchText(this.patientId, text, page - 1);
-        this.update(result);
-    }
-
-    createItem(textVisit){
-        let ele = $(this.itemTemplateHtml);
-        let map = parseElement(ele);
-        let compItem = new Item(ele, map, this.rest);
-        compItem.init();
-        compItem.set(textVisit);
-        return compItem;
-    }
-}
 

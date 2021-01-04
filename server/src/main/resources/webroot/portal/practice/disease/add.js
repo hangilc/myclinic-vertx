eb10b2b7caafc9d55b2a7d7cb12e71710adb689c
hangilc <hangil@chang.jp>
2021-01-04 20:30:06 +0900

    temporary saving

--- server/src/main/resources/webroot/portal/practice/disease/add.js
+++ server/src/main/resources/webroot/portal/practice/disease/add.js
@@ -15,16 +15,16 @@ let tmpl = `
             <input type="text" size="1" class="x-month" /> <span class="x-month-label">月</span>
             <input type="text" size="1" class="x-day" /> <span class="x-day-label">日</span>
         </div>
-        <div class="mt-1">
-            <button type="button" class="x-enter btn btn-primary btn-sm">入力</button>
-            <a href="javascript:void(0)" class="x-susp">の疑い</a>
+        <div class="mt-1 mb-2 d-flex align-items-center">
+            <button type="button" class="x-enter btn btn-primary btn-sm mr-2">入力</button>
+            <a href="javascript:void(0)" class="mr-2 x-susp">の疑い</a>
             <a href="javascript:void(0)" class="x-del-adj">修飾語削除</a>
         </div>
         <form class="x-search-form mt-1">
-            <div class="align-middle">
-                <input type="text" class="x-search-text form-control border" size="7" />
-                <button type="submit" class="btn btn-secondary btn-sm mt-1 border">検索</button>
-                <a href="javascript:void(0)" class="x-example mt-1 ml-2 border">例</a>
+            <div class="form-inline">
+                <input type="text" class="x-search-text form-control mr-2" size="11" />
+                <button type="submit" class="btn btn-secondary btn-sm mr-2">検索</button>
+                <a href="javascript:void(0)" class="x-example">例</a>
             </div>
             <div class="mt-1" onsubmit="return false;">
                 <input type="radio" name="search-kind" class="x-disease-radio" checked> 病名

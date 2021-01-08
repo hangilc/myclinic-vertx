a0d0d17d7e4fa42e0ff75cc52596100688335b71
Hangil Chang <hangil@chang.jp>
2021-01-08 16:35:19 +0900

    adding record wqueue

--- server/src/main/resources/webroot/portal-reception/record/record-panel.js
+++ server/src/main/resources/webroot/portal-reception/record/record-panel.js
@@ -1,6 +1,7 @@
 import {createElementFrom} from "../../js/create-element-from.js";
 import {parseElement} from "../../js/parse-node.js";
 import {click} from "../../js/dom-helper.js";
+import {WqueueWidget} from "./wqueue-widget.js";
 
 let tmpl = `
 <div>
@@ -10,19 +11,20 @@ let tmpl = `
     <div class="row">
         <div class="col-9 x-main-pane"></div>
         <div class="col-3 x-side-pane">
-            <div class="dropdown">
+            <div class="dropdown mb-3">
                 <button class="btn btn-secondary dropdown-toggle" type="button"
                         data-toggle="dropdown" aria-haspopup="true"
                         aria-expanded="false">
                     患者選択
                 </button>
                 <div class="dropdown-menu x-menu" aria-labelledby="dropdownMenuButton">
-                    <a href="javascript:void(0)" class="x-wqueue dropdown-item mx-2">受付患者選択</a>
+                    <a href="javascript:void(0)" class="x-wqueue dropdown-item mx-2">受付患者</a>
                     <a href="javascript:void(0)" class="x-search dropdown-item mx-2">患者検索</a>
                     <a href="javascript:void(0)" class="x-recent dropdown-item mx-2">最近の診察</a>
                     <a href="javascript:void(0)" class="x-by-date dropdown-item mx-2">日付別</a>
                 </div>
             </div>
+            <div class="x-side-pane-workarea"></div>
         </div>
     </div>
 </div>
@@ -30,8 +32,9 @@ let tmpl = `
 
 export class RecordPanel {
     constructor(rest) {
+        this.rest = rest;
         this.ele = createElementFrom(tmpl);
-        this.map = parseElement(this.ele);
+        const map = this.map = parseElement(this.ele);
         click(map.wqueue, async event => await this.doWqueue());
         click(map.search, async event => await this.doSearch());
         click(map.recent, async event => await this.doRecent());
@@ -39,7 +42,9 @@ export class RecordPanel {
     }
 
     async doWqueue(){
-
+        const patients = (await this.rest.listWqueueFull()).map(df => df.patient);
+        const w = new WqueueWidget(patients);
+        this.setWorkarea(w.ele);
     }
 
     async doSearch(){
@@ -52,6 +57,12 @@ export class RecordPanel {
 
     async doByDate(){
 
+    }
+
+    setWorkarea(e){
+        let workarea = this.map.sidePaneWorkarea;
+        workarea.innerHTML = "";
+        workarea.append(e);
     }
 
 }
---
+++ server/src/main/resources/webroot/portal-reception/record/wqueue-widget.js
@@ -0,0 +1,36 @@
+import {Widget} from "../../js/widget.js";
+import {createElementFrom} from "../../js/create-element-from.js";
+import {parseElement} from "../../js/parse-node.js";
+
+export class WqueueWidget extends Widget {
+    constructor(patients) {
+        super();
+        this.patients = patients;
+        this.setTitle("受付患者");
+        this.updateItems();
+    }
+
+    updateItems(){
+        const e = this.getBody();
+        e.innerHTML = "";
+        this.patients.forEach(patient => {
+            const item = new Item(patient);
+            e.append(item.ele);
+        })
+    }
+}
+
+const itemTmpl = `
+    <div>
+        <a href="javascript:void(0)" class="x-label"></a>
+    </div>
+`;
+
+class Item {
+    constructor(patient){
+        this.ele = createElementFrom(itemTmpl);
+        const map = parseElement(this.ele);
+        const patientIdRep = `${patient.patientId}`.padStart(4, "0");
+        map.label.innerText = `(${patientIdRep}) ${patient.lastName}${patient.firstName}`;
+    }
+}

3175ea0cf46912114d32c9eea71bc4d82b4e2e1a
hangilc <hangil@chang.jp>
2021-05-18 23:46:56 +0900

    added backup-db-to-s3.sh

--- bin/backup-db-to-s3.sh
+++ bin/backup-db-to-s3.sh
@@ -1,5 +1,11 @@
 stamp=$(date +'%Y%m%d')
-fname="myclinic-backup-$stamp.sql"
+sqlfile="myclinic-backup-$stamp.sql"
+mysqldump -h $MYCLINIC_DB_HOST -u $MYCLINIC_DB_USER -p$MYCLINIC_DB_PASS --set-gtid-purged=OFF \
+	myclinic >/tmp/$sqlfile
+gzip /tmp/$sqlfile
+aws s3 cp /tmp/$sqlfile.gz s3://$MYCLINIC_DB_S3_BUCKET
+rm /tmp/$sqlfile.gz
 
-echo $fname
+
+
 

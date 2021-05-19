fc6bca6d09deba0e138f8eaa3c8d61bd4972a02b
hangilc <hangil@chang.jp>
2021-05-19 17:49:42 +0900

    added cancel appoint

--- appoint/src/main/java/dev/myclinic/vertx/appoint/AppointPersist.java
+++ appoint/src/main/java/dev/myclinic/vertx/appoint/AppointPersist.java
@@ -17,8 +17,16 @@ public class AppointPersist {
             stmt.setString(2, appoint.appointTime.toString());
             stmt.setString(3, appoint.patientName);
             stmt.setString(4, appoint.getAttrsAsJson(mapper));
-            int res = stmt.executeUpdate();
-            System.out.println(res);
+            stmt.executeUpdate();
+        }
+    }
+
+    public static void cancelAppoint(Connection conn, AppointDTO appoint) throws SQLException {
+        String sql = "delete from appoint where appoint_date = ? and appoint_time = ?";
+        try(PreparedStatement stmt = conn.prepareStatement(sql)){
+            stmt.setString(1, appoint.appointDate.toString());
+            stmt.setString(2, appoint.appointTime.toString());
+            stmt.executeUpdate();
         }
     }
 
--- camelcli/src/main/java/dev/myclinic/vertx/camelcli/Appoint.java
+++ camelcli/src/main/java/dev/myclinic/vertx/camelcli/Appoint.java
@@ -2,6 +2,7 @@ package dev.myclinic.vertx.camelcli;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.myclinic.vertx.appoint.AppointDTO;
+import dev.myclinic.vertx.camelcomp.appoint.CancelAppointProcessor;
 import dev.myclinic.vertx.camelcomp.appoint.EnterAppointProcessor;
 import dev.myclinic.vertx.db.MysqlDataSourceFactory;
 import dev.myclinic.vertx.db.MysqlDataSourceConfig;
@@ -21,6 +22,39 @@ public class Appoint {
     public static void main(String[] args) throws Exception {
         DataSource ds = MysqlDataSourceFactory.create(new MysqlDataSourceConfig());
         ObjectMapper mapper = new ObjectMapper();
+        //enter(ds, mapper);
+        cancel(ds);
+    }
+
+    private static void cancel(DataSource ds) throws Exception {
+        Connection conn = ds.getConnection();
+        conn.setAutoCommit(false);
+        CamelContext context = new DefaultCamelContext();
+        CompletableFuture<Void> done = new CompletableFuture<>();
+        context.addRoutes(new RouteBuilder(){
+            @Override
+            public void configure() throws Exception {
+                from("direct:start-cancel-appoint")
+                        .process(new CancelAppointProcessor(conn))
+                        .process(ex -> {
+                            conn.commit();
+                            conn.close();
+                        })
+                        .process(ex -> done.complete(null));
+            }
+        });
+        context.start();
+        AppointDTO appoint = new AppointDTO();
+        appoint.appointDate = LocalDate.of(2021, 6, 1);
+        appoint.appointTime = LocalTime.of(10, 0);
+        appoint.patientName = "診療太郎";
+        ProducerTemplate tmpl = context.createProducerTemplate();
+        tmpl.sendBody("direct:start-cancel-appoint", appoint);
+        done.join();
+        context.stop();
+    }
+
+    private static void enter(DataSource ds, ObjectMapper mapper) throws Exception {
         Connection conn = ds.getConnection();
         conn.setAutoCommit(false);
         CamelContext context = new DefaultCamelContext();
@@ -29,9 +63,6 @@ public class Appoint {
             @Override
             public void configure() throws Exception {
                 from("direct:start-enter-appoint")
-                        .process(ex -> {
-                            System.out.println("enter");
-                        })
                         .process(new EnterAppointProcessor(conn, mapper))
                         .process(ex -> {
                             conn.commit();

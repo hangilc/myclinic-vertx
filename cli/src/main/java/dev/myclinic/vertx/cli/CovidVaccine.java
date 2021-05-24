7d255e720b6985f722e62ecf809d69afe02eb6a1
hangilc <hangil@chang.jp>
2021-05-24 23:51:40 +0900

    added filter candidates

--- cli/src/main/java/dev/myclinic/vertx/cli/CovidVaccine.java
+++ cli/src/main/java/dev/myclinic/vertx/cli/CovidVaccine.java
@@ -2,6 +2,9 @@ package dev.myclinic.vertx.cli;
 
 import java.nio.file.Path;
 import java.util.List;
+import java.util.Objects;
+
+import static java.util.stream.Collectors.toList;
 
 public class CovidVaccine {
 
@@ -10,20 +13,56 @@ public class CovidVaccine {
     public static void main(String[] args) throws Exception {
         String stamp = Misc.makeTimestamp();
         System.out.println(stamp);
-        readRegularPatients();
+        List<RegularPatient> rps = readRegularPatients();
+        List<RegularPatient> candidates = rps.stream()
+                .filter(rp -> !rp.excluded && rp.age >= 65).collect(toList());
+        candidates.forEach(System.out::println);
+        System.out.println(candidates.size());
     }
 
     private static class RegularPatient {
         int patientId;
         String name;
+        int age;
         String phone;
+        boolean excluded;
+
+        @Override
+        public String toString() {
+            return "RegularPatient{" +
+                    "patientId=" + patientId +
+                    ", name='" + name + '\'' +
+                    ", age=" + age +
+                    ", phone='" + phone + '\'' +
+                    ", excluded=" + excluded +
+                    '}';
+        }
     }
 
     private static List<RegularPatient> readRegularPatients(){
         Path file = Path.of(CovidVaccineDir, "regular-patients.txt");
         List<String> lines = Misc.readLines(file.toString());
-        lines.forEach(System.out::println);
-        return null;
+        return lines.stream().map(CovidVaccine::parseRegularPatient)
+                .filter(Objects::nonNull).collect(toList());
+    }
+
+    private static RegularPatient parseRegularPatient(String line){
+        if( line == null || line.isEmpty() ){
+            return null;
+        } else {
+            RegularPatient rp = new RegularPatient();
+            char ch = line.charAt(0);
+            if( ch == 'x' ){
+                rp.excluded = true;
+                line = line.substring(1).stripLeading();
+            }
+            String[] parts = line.split("\\s+", 4);
+            rp.patientId = Integer.parseInt(parts[0]);
+            rp.name = parts[1];
+            rp.age = Integer.parseInt(parts[2]);
+            rp.phone = parts[3];
+            return rp;
+        }
     }
 
 }

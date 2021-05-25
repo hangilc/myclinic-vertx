2ded762036ffcfbcbcefaf95a69f3c8da6afcabe
Hangil Chang <hangil@chang.jp>
2021-05-25 13:02:54 +0900

    added to CovidVaccine

--- cli/src/main/java/dev/myclinic/vertx/cli/CovidVaccine.java
+++ cli/src/main/java/dev/myclinic/vertx/cli/CovidVaccine.java
@@ -4,20 +4,20 @@ import java.nio.file.Path;
 import java.util.List;
 import java.util.Objects;
 
+import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
 public class CovidVaccine {
 
-    private static String CovidVaccineDir = System.getenv("COVID_VACCINE_DIR");
+    private final static String CovidVaccineDir = System.getenv("COVID_VACCINE_DIR");
 
     public static void main(String[] args) throws Exception {
-        String stamp = Misc.makeTimestamp();
-        System.out.println(stamp);
         List<RegularPatient> rps = readRegularPatients();
-        List<RegularPatient> candidates = rps.stream()
+        List<RegularPatient> patients = rps.stream()
                 .filter(rp -> !rp.excluded && rp.age >= 65).collect(toList());
-        candidates.forEach(System.out::println);
-        System.out.println(candidates.size());
+        List<RegularPatient> over65Candidates = listOver65Candidates(patients);
+        saveOver65Candidates(over65Candidates);
+        saveOver65CandidatePatientIds(over65Candidates);
     }
 
     private static class RegularPatient {
@@ -44,6 +44,30 @@ public class CovidVaccine {
         List<String> lines = Misc.readLines(file.toString());
         return lines.stream().map(CovidVaccine::parseRegularPatient)
                 .filter(Objects::nonNull).collect(toList());
+    }
+
+    private static List<RegularPatient> listOver65Candidates(List<RegularPatient> patients){
+        return patients.stream().filter(
+                p -> !p.excluded && p.age >= 65
+        ).collect(toList());
+    }
+
+    private static void saveOver65Candidates(List<RegularPatient> candidates){
+        Path file = Path.of(CovidVaccineDir, "over65-candidates.txt");
+        List<String> lines = candidates.stream()
+                .map(p -> String.format("%d %s %s", p.patientId, p.name, p.phone))
+                .collect(toList());
+        Misc.saveLines(file.toString(), lines);
+    }
+
+    private static void saveOver65CandidatePatientIds(List<RegularPatient> candidates){
+        Path file = Path.of(CovidVaccineDir, "over65-candidate-patient-ids.txt");
+        String content = candidates.stream()
+                .map(p -> p.patientId)
+                .sorted()
+                .map(patientId -> String.format("%d", patientId))
+                .collect(joining(", "));
+        Misc.saveString(file.toString(), content);
     }
 
     private static RegularPatient parseRegularPatient(String line){

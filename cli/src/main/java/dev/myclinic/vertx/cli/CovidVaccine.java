9c739cf308c0a9de021776733dba18836f812d07
hangilc <hangil@chang.jp>
2021-05-30 12:01:00 +0900

    added logbook-from-list

--- cli/src/main/java/dev/myclinic/vertx/cli/CovidVaccine.java
+++ cli/src/main/java/dev/myclinic/vertx/cli/CovidVaccine.java
@@ -1,7 +1,12 @@
 package dev.myclinic.vertx.cli;
 
+import java.io.FileNotFoundException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.LocalDate;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
@@ -11,6 +16,123 @@ public class CovidVaccine {
     private final static String CovidVaccineDir = System.getenv("COVID_VACCINE_DIR");
 
     public static void main(String[] args) throws Exception {
+        if( args.length == 0 ){
+            printHelp();
+        } else {
+            String cmd = args[0];
+            switch (cmd) {
+                case "backup-logbook": backupLogbook(); break;
+                case "logbook-from-list": logbookFromList(); break;
+                case "help": // fall through
+                default:
+                    printHelp();
+                    break;
+            }
+        }
+    }
+
+    private static void printHelp(){
+        System.out.println("CovidVaccine COMMAND [ARGS..]");
+        System.out.println("  backup-logbook");
+        System.out.println("  logbook-from-list");
+        System.out.println("  help");
+    }
+
+    private static void logbookFromList() throws Exception {
+        Path listFile = Path.of(CovidVaccineDir, "regular-patients.txt");
+        if(Files.exists(listFile) ){
+            List<String> lines = Misc.readLines(listFile.toString());
+            List<RegularPatient> patients = lines.stream().map(CovidVaccine::parseRegularPatient)
+                    .filter(Objects::nonNull).collect(toList());
+            List<String> logs = new ArrayList<>();
+            for(RegularPatient p: patients){
+                logs.add(logbookAdd(p.patientId, p.name, p.age, p.phone));
+                parsePatientAttr(p.attr);
+                logs.add(logbookState(p.patientId, p.attr));
+            }
+            Path logbookPath = Path.of(CovidVaccineDir, "logbook.txt");
+            if( Files.exists(logbookPath) ){
+                System.err.printf("logbook (%s) already exists.\n", logbookPath.toString());
+                System.exit(1);
+            }
+            Misc.saveLines(logbookPath.toString(), logs);
+        } else {
+            throw new FileNotFoundException(listFile.toString());
+        }
+    }
+
+    private static String logbookAdd(int patientId, String name, int age, String phone){
+        return String.format("ADD %d %s %d %s", patientId, name, age, phone);
+    }
+
+    private static String logbookState(int patientId, String attr){
+        return String.format("STATE %d %s", patientId, attr);
+    }
+
+    private static void backupLogbook() throws Exception {
+        Path logbookPath = Path.of(CovidVaccineDir, "logbook.txt");
+        if( Files.exists(logbookPath) ){
+            Path archDir = Path.of(CovidVaccineDir, "arch");
+            Misc.ensureDirectory(archDir.toString());
+            String stamp = Misc.makeTimestamp();
+            Path backupPath = archDir.resolve(String.format("logbook-%s.txt", stamp));
+            Files.move(logbookPath, backupPath);
+            System.out.printf("%s moved to %s.\n ", logbookPath.toString(), backupPath.toString());
+        } else {
+            System.err.printf("logbook file (%s) does not exists.\n", logbookPath.toString());
+        }
+    }
+
+    private interface PatientState {};
+
+    private static class FirstShotCandidate implements PatientState {}
+
+    private static class SecondShotCandidate implements PatientState {
+        public static Pattern pat = Pattern.compile("S(\\d\\d)(\\d\\d)");
+        public LocalDate firstShotDate;
+
+        public SecondShotCandidate(LocalDate firstShotDate) {
+            this.firstShotDate = firstShotDate;
+        }
+    }
+
+    private static class NotCurrentCandidate implements PatientState {}
+
+    private static class WaitingReply implements PatientState {}
+
+    private static class NeedConfirm implements PatientState {}
+
+    private static class DoneAtOtherPlace implements PatientState {}
+
+    private static PatientState parsePatientAttr(String attr){
+        attr = attr.trim();
+        if( attr.length() > 0 ){
+            switch (attr) {
+                case "C":
+                    return new FirstShotCandidate();
+                case "x":
+                    return new NotCurrentCandidate();
+                case "P":
+                    return new WaitingReply();
+                case "*":
+                    return new NeedConfirm();
+                case "T":
+                    return new DoneAtOtherPlace();
+                default:
+                    Matcher m = SecondShotCandidate.pat.matcher(attr);
+                    if (m.matches()) {
+                        int month = Integer.parseInt(m.group(1));
+                        int day = Integer.parseInt(m.group(2));
+                        int year = LocalDate.now().getYear();
+                        return new SecondShotCandidate(LocalDate.of(year, month, day));
+                    }
+                    break;
+            }
+        }
+        throw new RuntimeException("Invalid attribute: " + attr);
+    }
+
+    public static void mainOrig(String[] args) throws Exception {
         List<RegularPatient> rps = readRegularPatients();
         List<RegularPatient> candidates = rps.stream()
                 .filter(rp -> !rp.isExcluded()).collect(toList());
@@ -47,8 +169,9 @@ public class CovidVaccine {
                 line = "* " + line;
             }
             String[] parts = line.split("\\s+", 5);
-            String attr = parts[0];
-            rp.attr = attr.equals("*") ? "" : attr;
+            //String attr = parts[0];
+            //rp.attr = attr.equals("*") ? "" : attr;
+            rp.attr = parts[0];
             rp.patientId = Integer.parseInt(parts[1]);
             rp.name = parts[2];
             rp.age = Integer.parseInt(parts[3]);

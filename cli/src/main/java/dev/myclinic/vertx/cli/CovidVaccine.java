c1013f1253177f731c218bd82e102df3a39d1791
Hangil Chang <hangil@chang.jp>
2021-05-31 16:48:33 +0900

    temporary saving

--- cli/src/main/java/dev/myclinic/vertx/cli/CovidVaccine.java
+++ cli/src/main/java/dev/myclinic/vertx/cli/CovidVaccine.java
@@ -57,6 +57,10 @@ public class CovidVaccine {
                 case "current":
                     current();
                     break;
+                case "patient-ids": {
+                    patientIds();
+                    break;
+                }
                 case "help": // fall through
                 default:
                     printHelp();
@@ -76,6 +80,7 @@ public class CovidVaccine {
         System.out.println("  list-patches");
         System.out.println("  apply-patches");
         System.out.println("  current");
+        System.out.println("  patient-ids");
         System.out.println("  help");
     }
 
@@ -84,15 +89,15 @@ public class CovidVaccine {
         Map<Integer, RegularPatient> map = new HashMap<>();
         patients.forEach(p -> map.put(p.patientId, p));
         List<String> patchLines = Misc.readLines(patchPath.toString());
-        for(String patchLine: patchLines){
+        for (String patchLine : patchLines) {
             PatchCommand cmd = parsePatch(patchLine);
-            if( cmd instanceof PatchAdd ){
+            if (cmd instanceof PatchAdd) {
                 PatchAdd pa = (PatchAdd) cmd;
                 System.out.printf("ADD %s\n", pa.patient.toString());
-            } else if( cmd instanceof PatchState ){
+            } else if (cmd instanceof PatchState) {
                 PatchState ps = (PatchState) cmd;
                 RegularPatient rp = map.get(ps.patientId);
-                if( rp == null ){
+                if (rp == null) {
                     throw new RuntimeException("Cannot find patient with patient-id: " + ps.patientId);
                 }
                 rp.attr = ps.attr;
@@ -103,7 +108,7 @@ public class CovidVaccine {
         }
         System.console().writer().print("Apply these patches? (Y/N)");
         String input = System.console().readLine();
-        if( input.startsWith("Y") ){
+        if (input.startsWith("Y")) {
             List<String> logs = Misc.readLines(logbookPath.toString());
             logs.addAll(patchLines);
             executeLogbook(logs);
@@ -112,17 +117,17 @@ public class CovidVaccine {
     }
 
     private static void listPatches() throws Exception {
-        if( Files.exists(patchPath) ){
+        if (Files.exists(patchPath)) {
             List<String> lines = Misc.readLines(patchPath.toString());
             lines.forEach(System.out::println);
         }
     }
 
     private static void registerPatch(String[] args) throws Exception {
-        if( args.length != 3 ){
+        if (args.length != 3) {
             System.out.println();
             System.err.print("Invalid patch: ");
-            for(int i=1;i<args.length;i++){
+            for (int i = 1; i < args.length; i++) {
                 System.err.print(args[i]);
                 System.err.print(" ");
             }
@@ -139,9 +144,9 @@ public class CovidVaccine {
     private static void dueSecondShot() throws Exception {
         List<RegularPatient> patients = executeLogbook();
         Map<LocalDate, List<RegularPatient>> map = new HashMap<>();
-        for(RegularPatient patient: patients){
+        for (RegularPatient patient : patients) {
             PatientState st = parsePatientAttr(patient.attr);
-            if( st instanceof SecondShotCandidate ){
+            if (st instanceof SecondShotCandidate) {
                 SecondShotCandidate ssc = (SecondShotCandidate) st;
                 LocalDate dueDate = ssc.firstShotDate.plus(3, ChronoUnit.WEEKS);
                 List<RegularPatient> list = map.computeIfAbsent(dueDate, k -> new ArrayList<>());
@@ -151,7 +156,7 @@ public class CovidVaccine {
         List<LocalDate> dates = new ArrayList<>(map.keySet());
         dates.sort(Comparator.naturalOrder());
         Map<Integer, String> yomiMap = readPatientYomi();
-        for(LocalDate d: dates){
+        for (LocalDate d : dates) {
             List<RegularPatient> list = map.get(d);
             list.sort(Comparator.comparing(p -> getPatientYomi(p.patientId, yomiMap)));
             list.forEach(p -> {
@@ -264,7 +269,10 @@ public class CovidVaccine {
         }
     }
 
-    private interface PatchCommand {};
+    private interface PatchCommand {
+    }
+
+    ;
 
     private static class PatchAdd implements PatchCommand {
         public RegularPatient patient;
@@ -284,7 +292,7 @@ public class CovidVaccine {
         }
     }
 
-    private static PatchCommand parsePatch(String patch){
+    private static PatchCommand parsePatch(String patch) {
         patch = patch.trim();
         String[] parts = patch.split("\\s+", 2);
         if (parts.length != 2) {
@@ -321,7 +329,7 @@ public class CovidVaccine {
         }
     }
 
-    private static List<RegularPatient> executeLogbook(){
+    private static List<RegularPatient> executeLogbook() {
         List<String> logs = Misc.readLines(logbookPath.toString());
         return executeLogbook(logs);
     }
@@ -334,14 +342,14 @@ public class CovidVaccine {
                 continue;
             }
             PatchCommand cmd = parsePatch(log);
-            if( cmd instanceof PatchAdd ){
+            if (cmd instanceof PatchAdd) {
                 PatchAdd pa = (PatchAdd) cmd;
                 RegularPatient p = pa.patient;
                 if (map.containsKey(p.patientId)) {
                     throw new RuntimeException("Duplicate ADD command: " + log);
                 }
                 map.put(p.patientId, p);
-            } else if( cmd instanceof PatchState ){
+            } else if (cmd instanceof PatchState) {
                 PatchState ps = (PatchState) cmd;
                 int patientId = ps.patientId;
                 String attr = ps.attr;
@@ -358,7 +366,7 @@ public class CovidVaccine {
         return new ArrayList<>(map.values());
     }
 
-    private static List<String> prepareList() throws Exception {
+    private static Map<String, List<RegularPatient>> prepareGroupsForListing() throws Exception {
         List<RegularPatient> patients = executeLogbook();
         Map<String, List<RegularPatient>> groups = new HashMap<>();
         patients.forEach(p -> {
@@ -375,9 +383,19 @@ public class CovidVaccine {
         keys.remove("x");
         keys.remove("U");
         codes.addAll(keys);
-        List<String> lines = new ArrayList<>();
-        Map<Integer, String> yomiMap = readPatientYomi();
+        Map<String, List<RegularPatient>> result = new LinkedHashMap<>();
         for (String code : codes) {
+            result.put(code, groups.get(code));
+        }
+        return result;
+    }
+
+
+    private static List<String> prepareList() throws Exception {
+        Map<String, List<RegularPatient>> groups = prepareGroupsForListing();
+        Map<Integer, String> yomiMap = readPatientYomi();
+        List<String> lines = new ArrayList<>();
+        for (String code : groups.keySet()) {
             List<RegularPatient> list = groups.getOrDefault(code, Collections.emptyList());
             list.sort(Comparator.comparing(p -> getPatientYomi(p.patientId, yomiMap)));
             list.forEach(p -> lines.add(p.toString()));
@@ -386,6 +404,35 @@ public class CovidVaccine {
         }
         return lines;
     }
+
+//    private static List<String> prepareList() throws Exception {
+//        List<RegularPatient> patients = executeLogbook();
+//        Map<String, List<RegularPatient>> groups = new HashMap<>();
+//        patients.forEach(p -> {
+//            String code = p.attr.substring(0, 1);
+//            List<RegularPatient> list = groups.computeIfAbsent(code, k -> new ArrayList<>());
+//            list.add(p);
+//        });
+//        List<String> codes = new ArrayList<>();
+//        Set<String> keys = new HashSet<>(groups.keySet());
+//        List.of("C", "S", "*", "P", "T").forEach(c -> {
+//            codes.add(c);
+//            keys.remove(c);
+//        });
+//        keys.remove("x");
+//        keys.remove("U");
+//        codes.addAll(keys);
+//        List<String> lines = new ArrayList<>();
+//        Map<Integer, String> yomiMap = readPatientYomi();
+//        for (String code : codes) {
+//            List<RegularPatient> list = groups.getOrDefault(code, Collections.emptyList());
+//            list.sort(Comparator.comparing(p -> getPatientYomi(p.patientId, yomiMap)));
+//            list.forEach(p -> lines.add(p.toString()));
+//            lines.add(String.format("(%d)", list.size()));
+//            lines.add("");
+//        }
+//        return lines;
+//    }
 
 
     private static void list() throws Exception {
@@ -397,6 +444,20 @@ public class CovidVaccine {
         List<String> lines = prepareList();
         Path currentPath = Path.of(CovidVaccineDir, "current.txt");
         Misc.saveLines(currentPath.toString(), lines);
+    }
+
+    private static void patientIds() throws Exception {
+        Map<String, List<RegularPatient>> groups = prepareGroupsForListing();
+        for(String code: groups.keySet()){
+            List<RegularPatient> list = groups.computeIfAbsent(code, k -> Collections.emptyList());
+            List<String> patientIds = list.stream()
+                    .sorted(Comparator.comparing(p -> p.patientId))
+                    .map(p -> String.format("%d", p.patientId))
+                    .collect(toList());
+            System.out.println(code);
+            System.out.println(String.join(", ", patientIds));
+            System.out.println();
+        }
     }
 
     private interface PatientState {
@@ -459,16 +520,6 @@ public class CovidVaccine {
         throw new RuntimeException("Invalid attribute: " + attr);
     }
 
-    public static void mainOrig(String[] args) throws Exception {
-        List<RegularPatient> rps = readRegularPatients();
-        List<RegularPatient> candidates = rps.stream()
-                .filter(rp -> !rp.isExcluded()).collect(toList());
-        List<RegularPatient> over65Candidates = candidates.stream()
-                .filter(rp -> rp.age >= 65).collect(toList());
-        saveOver65Candidates(over65Candidates);
-        saveOver65CandidatePatientIds(over65Candidates);
-    }
-
     private static class RegularPatient {
         int patientId;
         String name;
@@ -505,51 +556,6 @@ public class CovidVaccine {
             rp.phone = parts[4];
             return rp;
         }
-    }
-
-    private static List<RegularPatient> readRegularPatients() {
-        Path file = Path.of(CovidVaccineDir, "regular-patients.txt");
-        List<String> lines = Misc.readLines(file.toString());
-        return lines.stream().map(CovidVaccine::parseRegularPatient)
-                .filter(Objects::nonNull).collect(toList());
-    }
-
-    private static void saveOver65Candidates(List<RegularPatient> candidates) {
-        Map<String, List<RegularPatient>> groups = new HashMap<>();
-        candidates.forEach(p -> {
-            String code = p.attr;
-            if (code.length() > 0) {
-                code = code.substring(0, 1).toUpperCase();
-            }
-            List<RegularPatient> list = groups.computeIfAbsent(code, k -> new ArrayList<>());
-            list.add(p);
-        });
-        List<String> codes = new ArrayList<>();
-        Set<String> keys = new HashSet<>(groups.keySet());
-        List.of("C", "S", "", "T").forEach(c -> {
-            codes.add(c);
-            keys.remove(c);
-        });
-        codes.addAll(keys);
-        List<String> lines = new ArrayList<>();
-        for (String code : codes) {
-            List<RegularPatient> list = groups.getOrDefault(code, Collections.emptyList());
-            list.forEach(p -> lines.add(p.toString()));
-            lines.add(String.format("(%d)", list.size()));
-            lines.add("");
-        }
-        Path file = Path.of(CovidVaccineDir, "over65-candidates.txt");
-        Misc.saveLines(file.toString(), lines);
-    }
-
-    private static void saveOver65CandidatePatientIds(List<RegularPatient> candidates) {
-        Path file = Path.of(CovidVaccineDir, "over65-candidate-patient-ids.txt");
-        String content = candidates.stream()
-                .map(p -> p.patientId)
-                .sorted()
-                .map(patientId -> String.format("%d", patientId))
-                .collect(joining(", "));
-        Misc.saveString(file.toString(), content);
     }
 
 }

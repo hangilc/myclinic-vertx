9caa50f35adadad63c17e5df0479e15a88bcfda4
Hangil Chang <hangil@chang.jp>
2021-06-14 17:21:29 +0900

    temporary saving

--- cli/src/main/java/dev/myclinic/vertx/cli/covidvaccine/CovidVacServer.java
+++ cli/src/main/java/dev/myclinic/vertx/cli/covidvaccine/CovidVacServer.java
@@ -2,6 +2,8 @@ package dev.myclinic.vertx.cli.covidvaccine;
 
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerOptions;
+import io.vertx.core.net.PemKeyCertOptions;
 import io.vertx.ext.web.Router;
 
 public class CovidVacServer {
@@ -9,7 +11,17 @@ public class CovidVacServer {
     public static void main(String[] args){
         Vertx vertx = Vertx.vertx();
         Router router = Router.router(vertx);
-        HttpServer server = vertx.createHttpServer();
+        router.route("/")
+        String certPath = System.getenv("MYCLINIC_SERVER_CERT");
+        String privateKey = System.getenv("MYCLINIC_SERVER_PRIVATE_KEY");
+        HttpServer server = vertx.createHttpServer(new HttpServerOptions()
+                .setPemKeyCertOptions(new PemKeyCertOptions()
+                        .setCertPath(certPath)
+                        .setKeyPath(privateKey)
+                )
+                .setSsl(true)
+        );
+        server.requestHandler(router);
     }
 
 }

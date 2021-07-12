08974c9f024697bbf0384a246d9f2f579202773a
Hangil Chang <hangil@chang.jp>
2021-07-12 17:00:13 +0900

    added appointdb get connection

--- cli/src/main/java/dev/myclinic/vertx/cli/appoint/AppointMisc.java
+++ cli/src/main/java/dev/myclinic/vertx/cli/appoint/AppointMisc.java
@@ -1,6 +1,8 @@
 package dev.myclinic.vertx.cli.appoint;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 class AppointMisc {
 
@@ -8,8 +10,17 @@ class AppointMisc {
 
     }
 
-    public static Connection openConnection(){
-        throw new RuntimeException("Not implemented.");
+    public static Connection openConnection() {
+        String dbFile = System.getenv("MYCLINIC_APPOINT_DB");
+        if( dbFile == null ){
+            throw new RuntimeException("Cannot find env var: MYCLINIC_APPOINT_DB");
+        }
+        String url = String.format("jdbc:sqlite:%s", dbFile);
+        try {
+            return DriverManager.getConnection(url);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
 }
